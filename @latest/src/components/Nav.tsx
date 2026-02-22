2887f6875897fe40b62d7e7c9d66b0325f43395e
Oarabile Mbewe <oarabilembewe@DVTLUNLVCH.local>
2026-02-22 07:39:12 +0200

    Added nav styling

--- @latest/src/components/Nav.tsx
+++ @latest/src/components/Nav.tsx
@@ -1,7 +1,8 @@
+
 export default function NavBar(){
     return(
         <div className="Nav_Container">
-            <nav className="navbar navbar-expand-lg navbar-light">
+            <nav className="navbar navbar-expand-lg custom-navbar">
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
